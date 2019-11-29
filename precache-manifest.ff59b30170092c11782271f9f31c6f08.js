f6290149b7d4c1b232576cd3527a3175edc8a055
Busry <minitext@gmail.com>
2019-11-29 15:04:50 +0000

    Updates

---
+++ precache-manifest.ff59b30170092c11782271f9f31c6f08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8b25351604752ec783849a6c01e32f",
+    "url": "/teamwork_frontend/index.html"
+  },
+  {
+    "revision": "91f16944b7ce6de842c7",
+    "url": "/teamwork_frontend/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a25520ba5974bd1555fe",
+    "url": "/teamwork_frontend/static/js/2.1a23e3e7.chunk.js"
+  },
+  {
+    "revision": "91f16944b7ce6de842c7",
+    "url": "/teamwork_frontend/static/js/main.63ce0137.chunk.js"
+  },
+  {
+    "revision": "8cd2daf474abcfe869e7",
+    "url": "/teamwork_frontend/static/js/runtime-main.ad217419.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/teamwork_frontend/static/media/logo.25bf045c.svg"
+  }
+]);
